2e6e433326567a19a290a1d9180424c714308440
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-27 06:22:56 -0500

    Messed with lists

--- CSharpForBPASolution/Basics/Program.cs
+++ CSharpForBPASolution/Basics/Program.cs
@@ -1,8 +1,14 @@
-﻿internal class program {
+﻿using System.Collections.Generic;
+
+internal class program {
     static void Main(string[] args) {
-        int[] test = {1,2,3,4,5};
-        for (int i = 0; i<test.Length; i++) {
-            Console.WriteLine(Convert.ToString(i) + " " + test[i]);
+        List<string> test = new List<string>();
+        test.Add("1");
+        test.Add("2");
+        test.Add("3");
+        test.Add("4");
+        for (int i=0; i<test.Count; i++) {
+            Console.WriteLine(test[i]);
         }
     }
 }
