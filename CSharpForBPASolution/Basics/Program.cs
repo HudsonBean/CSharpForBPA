32568e3ed9deb9c26bc1999849a7790b120c8ccd
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-27 06:16:16 -0500

    Messed with arrays

--- CSharpForBPASolution/Basics/Program.cs
+++ CSharpForBPASolution/Basics/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿internal class program {
+    static void Main(string[] args) {
+        int[] test = {1,2,3,4,5};
+        for (int i = 0; i<test.Length; i++) {
+            Console.WriteLine(Convert.ToString(i) + " " + test[i]);
+        }
+    }
+}
